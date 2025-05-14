470b3b7411f5197f6aa6261896a15aeeffda5e47
Tomas Yanez <45236504+tomasyanezg@users.noreply.github.com>
2025-05-14 15:31:55 +0000

    Part1c: Last UDT declared with other UDTs

--- main.cpp
+++ main.cpp
@@ -350,17 +350,17 @@ Define an object that is made of 5 sub-objects.
     These 5 sub-objects will not be defined using Primitives, but instead will be their own UDTs 
     you'll define these 5 sub-objects in Part 1d.
 
-Thing 10)
+Thing 10) Restaurant
 5 properties:
-    1)
-    2)
-    3)
-    4)
-    5)
+    1) employees
+    2) customers
+    3) main dishes 
+    4) desserts
+    5) beverages
 3 things it can do:
-    1)
-    2)
-    3)
+    1) serve food and drinks
+    2) reserve a table for x customers
+    3) charge customers in cash or with card
  */
 
  /*
